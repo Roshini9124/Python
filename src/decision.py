a54232b382ca805921b79992e2b916daa7e078c7
Roshini9124 <narayananroshini9@gmail.com>
2026-07-01 15:09:15 +0530

    Decision making statements

---
+++ src/decision.py
@@ -0,0 +1,13 @@
+mark=49
+grade=None
+if(mark>90):
+    grade='O'
+elif(mark>=80 and mark<=90):
+    grade='A+'
+elif(mark>=70 and mark <=79):
+    grade='A'
+elif(mark>=60 and mark<=69):
+    grade='B+'
+elif(mark>=50 and mark<=59):
+    grade='B'
+print(grade)
