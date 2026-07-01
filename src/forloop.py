c70687f67be201c267994fc0292787acde6caf1d
Roshini9124 <roshini.n@diggibyte.com>
2026-07-01 12:07:57 +0530

    Fix for loop structure and print statements

--- src/forloop.py
+++ src/forloop.py
@@ -8,13 +8,13 @@ a=[1,2,3,45,7]
 '''for i in range(len(a)-1,-1,-1):
     print(i,a[i])
 '''
-for i in reversed(a):
+'''for i in reversed(a):
     if i==3:
         print(i)
         break
 
 else:
-    print("no break")
+    print("no break")'''
 
 '''for i in range(-1,-(len(a)+1),-1):
     print(i,a[i])'''
