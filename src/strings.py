56c2b867118014de56fb7ff18dd876060c2e5d91
Roshini9124 <narayananroshini9@gmail.com>
2026-07-01 15:22:51 +0530

    String concepts

---
+++ src/strings.py
@@ -0,0 +1,26 @@
+a="programming python   22"
+
+'''for i in a:
+    print(i)'''
+
+''''print("\n",a[0])
+print(a[3:])
+print(a[:2])
+print(a[::-1])'''
+
+'''print(a.upper())
+print(a.lower())
+print(a.title())
+print(a.capitalize())
+print(a.strip())
+a=a.replace("py","Py")
+print(a)
+print(a.isupper())
+print(a.islower())
+print(a.isalpha())
+print(a.isdigit())
+print(a.isalnum())
+'''
+
+for i,j in enumerate(a):
+    print(i,j)
