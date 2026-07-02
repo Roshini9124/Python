7b8e43a7dadb61e49c942878cb8e97d93bf731dd
Roshini9124 <narayananroshini9@gmail.com>
2026-07-02 22:47:53 +0530

    File operations

---
+++ src/files.py
@@ -0,0 +1,17 @@
+f1=open("info.txt","w")
+count=f1.write("this")  #write method returns no.of.character it wrote
+print(count)
+'''list=[1,2,3,45]
+for i in list:
+    f1.write(str(i)+"\n")
+c2=f1.writelines(str(list))  #writelines return none
+print(c2)  '''
+f1=open("info.txt","r")
+#print(f1.read())
+print(f1.readline())
+#print(f1.readline())
+print(f1.tell())
+f1.seek(2)
+print(f1.read())
+print(f1.tell())
+f1.close()
