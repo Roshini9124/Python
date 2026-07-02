7f3f45da7e2092edc5438d49c679aaf90f68c5b8
Roshini9124 <narayananroshini9@gmail.com>
2026-07-02 14:50:57 +0530

    Dictioary operations

---
+++ src/dictt.py
@@ -0,0 +1,24 @@
+d={
+    1:"Emp1",
+    2:"Emp2",
+    3:"Emp3",
+    
+    }
+
+print(d)
+'''for i in d.values():
+    print(i)
+
+for i in d.items():
+    print(i)
+
+for i in d:
+    print(i)
+
+for i in d.keys():
+    print(i)'''
+
+d[1]="Roshini"
+print(d)
+
+print(d.get(4))
