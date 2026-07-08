33c5fc48cedc5f09a317287567d8b062e147c8e0
Roshini9124 <roshini.n@diggibyte.com>
2026-07-08 10:28:37 +0530

    unit test practice

--- src/test_emp.py
+++ src/test_emp.py
@@ -1,16 +1,19 @@
 from emp import Employee
-
-def test_cons():
-    ob=Employee("Joey",90000)
+import pytest
+@pytest.fixture()
+def ob():
+    return Employee("Joey",90000)
+def test_cons(ob):
+    #ob=Employee("Joey",90000)
     assert ob.name=="Joey"
     assert ob.salary==90000
 
-def test_sal():
-    ob=Employee("Joey",90000)
+def test_sal(ob):
+    #ob=Employee("Joey",90000)
     
     assert ob.increment_salary(500)== 90500
 
-def test_changename():
-    ob=Employee("Joey",90000)
+def test_changename(ob):
+    #ob=Employee("Joey",90000)
     ob.change_name("Ken Adams")
     assert ob.name=="Ken Adams"
--- src/test_maths.py
+++ src/test_maths.py
@@ -2,7 +2,8 @@ import pytest
 from maths import *
 def test_add():
     assert add(5,4)==9
-    assert add(-1,-4)==-8
+    assert add(-1,-4)==-5
+    assert sub(9,11)==2
 
 
     '''Unit testing is a software testing technique where we test the smallest individual parts (units) of a program separately to verify that they work correctly.
@@ -14,3 +15,9 @@ A method
 A class
 
 Instead of testing the entire application at once, we test each small component independently.'''
+
+'''A fixture in pytest is a function that prepares some data or environment before a test runs and cleans it up afterward (if needed).
+
+Think of a fixture as a helper function that provides resources to your tests.
+
+Instead of writing the same setup code in every test, you write it once in a fixture and reuse it.'''
